40c12ede930cb832dcc07472478b3bb222456e6c
romansherwood <sherwoodroma777@gmail.com>
2020-11-03 22:09:23 +0300

    5.7. Ипотека [#395744]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100;
+        while (amount > 0) {
+            amount = (int) ((amount * percent + amount) - salary);
+            year++;
+        }
+        return year;
+    }
+}
