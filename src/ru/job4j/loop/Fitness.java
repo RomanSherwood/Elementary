d7b9c650ec42535c449768a912bbd792512dfad7
romansherwood <sherwoodroma777@gmail.com>
2020-11-03 21:25:36 +0300

    5.4. Протеиновая диета [#395742] Метод упрощен

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,13 +3,10 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
             month++;
-        }
-        if (ivan == nik) {
-            month = 1;
         }
         return month;
     }
