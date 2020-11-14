e366c9a68db4bd319636f0a0cad44bb6ddc2e436
romansherwood <sherwoodroma777@gmail.com>
2020-11-14 11:50:06 +0300

    6.7.1. Моно строка в матрице. [#395663] Поправлен метод

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -4,8 +4,7 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            char equal = board[row][0];
-            if (board[row][i] != equal) {
+            if (board[row][i] != 'X') {
                 return false;
             }
         }
